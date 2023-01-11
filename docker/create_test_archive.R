6f3ed42d6c26e9a17c33bfa677d543399b15aa82
Alex Hill <alex.hill@gmail.com>
2023-01-11 16:06:50 +0000

    cron working and tested

--- docker/create_test_archive.R
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env Rscript
-
-arguments <- commandArgs(trailingOnly = TRUE)
-orderly::orderly_example("demo", path = arguments[[1]],
-                         run_demo = FALSE, quiet = TRUE)
--- orderly2outpack.R
+++ orderly2outpack.R
@@ -3,11 +3,11 @@
 'orderly2outpack.
 
 Usage:
-  orderly2outpack.R <src> <dest> [--once] [--hourly]
+  orderly2outpack.R <src> <dest> [--once] [--custom=<schedule>]
 
 Options:
-  --once      Perform migration once.
-  --hourly    Schedule hourly migration.
+  --once                     Perform migration once.
+  --custom=<schedule>   Schedule migration with provided cron schedule.
 
 ' -> doc
 
@@ -15,10 +15,13 @@ arguments <- docopt::docopt(doc)
 
 if (arguments$once) {
     outpack.orderly::orderly2outpack(arguments$src, arguments$dest)
-} else if (arguments$hourly) {
-    write(c("#!/usr/bin/env Rscript", "\n", deparse({
-        outpack.orderly::orderly2outpack(arguments$src, arguments$dest)
-    })), "~/outpack.hourly")
+} else if (!is.null(arguments$custom)) {
+    fn <- deparse(substitute(outpack.orderly::orderly2outpack(src, dest),
+                             list(src=arguments$src, dest=arguments$dest)))
+    write(c("#!/usr/bin/env Rscript", "\n", fn), "~/orderly2outpack")
+    freq <- arguments$custom
+    crontab <- "/usr/local/bin/Rscript /home/outpack/orderly2outpack >> /home/outpack/cron.log 2>&1"
+    write(c("SHELL=/bin/bash", paste(freq, crontab)), "~/tab.txt")
 } else {
-    stop("Frequency of migration not specified. Please pass either --once or --hourly option.")
+    stop("Frequency of migration not specified. Please pass either --once or --custom option.")
 }
